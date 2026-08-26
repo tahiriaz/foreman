e57132deaca6f035a0d549e90b802971efcf88da
Tahir Riaz <tahiriaz.ahmed@gmail.com>
2026-08-26 14:52:19 +0300

    Update

--- configure_ilo_BL.py
+++ configure_ilo_BL.py
@@ -85,7 +85,7 @@ class TaskStatus(Enum):
 # ============================================================================
 
 OA_USERNAME = "Administrator"
-OA_PASSWORD = "Th@les01"
+OA_PASSWORD = "Siljeddah15"
 
 ILO_LOGIN = "thlocaladmin"
 ILO_PASSWORD = "Th@les018664"
